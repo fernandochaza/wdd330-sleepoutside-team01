dd46f0b6dd14db150865d4e955a1e09a66d42986
Lawrence9092 <lngwenya@byupathway.edu>
2026-03-17 15:43:48 +0200

    Created product alerts

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -1,4 +1,4 @@
-async function loadAlerts() {
+/*async function loadAlerts() {
     const response = await fetch("/public/json/alerts.json");
     const alerts = await response.json();
 
@@ -19,3 +19,32 @@ async function loadAlerts() {
 }
 
 loadAlerts();
+
+*/
+export default class Alerts {
+  constructor(url) {
+    this.url = url;
+  }
+
+  async init() {
+    const alerts = await this.getalerts();
+    if (!alerts || alerts.length === 0) return;
+    const section = document.createElement("section");
+    section.classList.add("alert-list");
+    alerts.forEach((alert) => {
+      const p = document.createElement("p");
+      p.textContent = alert.message;
+      p.style.backgroundColor = alert.background;
+      p.style.color = alert.color;
+      section.appendChild(p);
+    });
+    const main = document.querySelector("main");
+    main.prepend(section);
+  }
+
+  async getalerts() {
+    const response = await fetch(this.url);
+    const alerts = await response.json();
+    return alerts;
+  }
+}
