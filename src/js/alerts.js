bc7525f5e107c1372ea710b098b0a3e901794711
AbelChiwandire <achiwandire1@byupathway.edu>
2026-03-18 18:11:47 +0200

    refactor: implement dynamic header and footer

--- src/js/alerts.js
+++ src/js/alerts.js
@@ -1,26 +1,3 @@
-/*async function loadAlerts() {
-    const response = await fetch("/public/json/alerts.json");
-    const alerts = await response.json();
-
-    const container = document.getElementById("alert-container");
-
-    alerts.forEach(alert => {
-        const alertBox = document.createElement("div");
-
-        alertBox.textContent = alert.message;
-        alertBox.style.backgroundColor = alert.background;
-        alertBox.style.padding = "10px";
-        alertBox.style.color = "white";
-        alertBox.style.textAlign = "center";
-        alertBox.style.marginBottom = "5px";
-
-        container.appendChild(alertBox);
-    });
-}
-
-loadAlerts();
-
-*/
 export default class Alerts {
   constructor(url) {
     this.url = url;
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { updateCartCount } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import Alerts from "./alerts.js";
+
+loadHeaderFooter();
 
 const alerts = new Alerts("/json/alerts.json");
 alerts.init();
-// Update cart count on page load
-updateCartCount();
 
 const productData = new ProductData("tents");
 const productList = new ProductList(
