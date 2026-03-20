f8103b9c50282de080ea83e34de6a52713865e0a
AbelChiwandire <achiwandire1@byupathway.edu>
2026-03-20 16:54:53 +0200

    fix: build error

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,14 @@
 import ShoppingCart from "./ShoppingCart.mjs";
 import { loadHeaderFooter } from "./utils.mjs";
 
-await loadHeaderFooter();
+async function initCart() {
+  await loadHeaderFooter();
 
-const cartContainer = document.querySelector(".product-list");
-const cartFooter = document.querySelector(".cart-footer");
+  const cartContainer = document.querySelector(".product-list");
+  const cartFooter = document.querySelector(".cart-footer");
 
-const cart = new ShoppingCart(cartContainer, cartFooter);
-cart.renderCartContents();
+  const cart = new ShoppingCart(cartContainer, cartFooter);
+  cart.renderCartContents();
+}
+
+initCart();
